fe9c4f3f794c9e06ae31cc6f4ed46bac2f8e08b4
Frank Y <franklinyang@gmail.com>
2013-07-06 23:35:25 +0000

    toString issue

--- web.js
+++ web.js
@@ -6,9 +6,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  fs.ReadFile('index.html', function (err, data) {
+  fs.readFileSync('index.html', function (err, data) {
     if (err) throw err;
-    response.send(data);
+    response.send(data.toString('utf-8'));
   });
 
 //  response.send('Hello World 2!');
