c23d0f7614b1d2eef0daa1e92553b2fddb0f0fe8
Frank Y <franklinyang@gmail.com>
2013-07-06 23:25:58 +0000

    updated with index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  fs.ReadFile('index.html', function (err, data) {
+    if (err) throw err;
+    response.send(data);
+  });
+
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
